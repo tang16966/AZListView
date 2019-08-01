cb9946e07dcfdbf3727f0e3a292517ab519a39ab
tang <1914252033@qq.com>
2019-08-01 20:41:01 +0800

    adpter

--- azlistview/src/main/java/com/trs/azlist2/AZListView.java
+++ azlistview/src/main/java/com/trs/azlist2/AZListView.java
@@ -5,8 +5,12 @@ import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
 public class AZListView extends FrameLayout {
+    private ContactIndexView contactIndexView;
+
     public AZListView(Context context, AttributeSet attrs) {
         super(context, attrs);
         inflate(getContext(),R.layout.layout_az_view,this);
+        contactIndexView = findViewById(R.id.contact_index_view);
+
     }
 }
---
+++ azlistview/src/main/java/com/trs/azlist2/MyListView.java
@@ -0,0 +1,14 @@
+package com.trs.azlist2;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+public class MyListView extends ListView {
+
+    public MyListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
